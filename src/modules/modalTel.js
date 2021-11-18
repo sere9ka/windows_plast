e4a3a725cbb610f0afb80442655b84d9b3e89844
sere9ka <s.kovylov@zhiet.ru>
2021-11-18 10:23:43 +0300

    переписал функцию, работает так же на вызов замерщика

--- src/modules/modalTel.js
+++ src/modules/modalTel.js
@@ -1,26 +1,28 @@
-const modalTel = () => {
+const modal = () => {
     // отслеживаем события
     const body = document.querySelector('body')
     const modalForTel = document.querySelector('.header-modal--opened')
+    const modalForMan = document.querySelector('.services-modal--opened')
     const overlay = document.querySelector('.overlay')
 
     // функция - переключатель
-    const toggleModalTel = (e) => {
-        modalForTel.classList.toggle('header-modal')
+    const toggleModalTel = (toggleClass, targetElem) => {
         overlay.classList.toggle('overlay-open')
+        targetElem.classList.toggle(toggleClass)
     }    
 
     // отслеживаем клики по кнопке + запуск переключателя
     body.addEventListener('click', (e) => {
-            if (e.target.matches('.fancyboxModal')) {
-                toggleModalTel()
-            } else if (e.target.matches('.header-modal__close')) {
-                toggleModalTel()
-            }
+        e.preventDefault()
+        if (e.target.matches('.callTel') || e.target.matches('.header-modal__close')) {
+            toggleModalTel('header-modal', modalForTel)
+        } else if (e.target.matches('.modalCall') || e.target.matches('.services-modal__close')) {
+            toggleModalTel('services-modal', modalForMan)
+        }
     })
     
     
     
 }
 
-export default modalTel
+export default modal
