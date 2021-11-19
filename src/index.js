2b9806823a19d59068c3e8b689b88f41c2fd3e6a
sere9ka <s.kovylov@zhiet.ru>
2021-11-19 11:44:19 +0300

    сделана отправка форм

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import timer from './modules/timer'
 import scrollTo from './modules/scrollTo'
 import calc from './modules/calc'
 import validator from './modules/validator'
+import sendForm from './modules/sendForm'
 
 
 modal()
@@ -10,3 +11,22 @@ timer('24 november 2021')
 scrollTo()
 calc()
 validator()
+
+const forms = document.querySelectorAll('form')
+forms.forEach((form, id) => {
+    const statusBlock = document.createElement('div');
+    statusBlock.classList.add('status')
+    form.append(statusBlock)
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        sendForm({ 
+            formId: `form${id+1}`,
+            someElem: [
+                {
+                    type: 'block',
+                    id: 'calc-total'
+                }
+            ]
+         })
+    })
+})
--- src/modules/validator.js
+++ src/modules/validator.js
@@ -1,9 +1,23 @@
 const validator = () => {
-    const numberRules = /(^0-9)/
+    const phones = document.querySelectorAll('[name="phone"]')
+    const nameUsers = document.querySelectorAll('[name="fio"]')
+    const numberRules = /(^0-9)/;
+    const phoneRules = /([^0-9\+])/;
+    const nameRules = /[^А-Я-а-яё\s][^A-Z-a-z\s]+$/i;
     
     if (document.body.className == 'balkony') {
         document.querySelector('#calc-input').addEventListener('input', (e=>{e.target.value=e.target.value.replace(numberRules, "")}))
     }
+    nameUsers.forEach(nameUser => {
+        nameUser.addEventListener('input', (e=>{
+            e.target.value = e.target.value.replace(nameRules, "")
+        }))
+    })
+    phones.forEach(phone => {
+        phone.addEventListener('input', (e=>{
+            e.target.value = e.target.value.replace(phoneRules, "")
+        }))
+    })
 }
 
 export default validator
