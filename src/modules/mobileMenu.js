40ceae3c8738403a87a3632e4220c1a146028498
sere9ka <s.kovylov@zhiet.ru>
2021-11-25 10:04:34 +0300

    условия для меню по ширине

--- src/modules/mobileMenu.js
+++ src/modules/mobileMenu.js
@@ -1,11 +1,12 @@
 const mobileMenu = () => {
     const btnsNavbar = document.querySelectorAll('.navbar-toggle')
     const navBar = document.getElementById('navbar-collapse')
-
+    const width = document.documentElement.clientWidth
     
 
     const getToMenu = (e) => {
-        navBar.classList.remove('navbar-collapse', 'collapse')
+        navBar.classList.toggle('collapse')
+        navBar.classList.toggle('navbar-collapse')
         // .navbar-collapse.collapse
     }
 
@@ -16,6 +17,10 @@ const mobileMenu = () => {
             getToMenu(e)
         })
     })
+
+    if (width >= 992) {
+        getToMenu()
+    } else return
 }
 
 export default mobileMenu
